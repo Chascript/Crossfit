adffe32198230d3f2069d59ebc4d551643118886
Conan Chambers <conan@blinkseo.co.uk>
2024-02-10 12:44:04 +0000

    exerciseCard component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { NextUIProvider } from '@nextui-org/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <NextUIProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </NextUIProvider>
   );
 }
