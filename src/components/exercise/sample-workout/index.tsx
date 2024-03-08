0128a2dbbd31549164e9495f5128f35c44a03935
Conan Chambers <conan@blinkseo.co.uk>
2024-03-08 17:01:16 +0000

    finishing off components, deleted files as using BulletList component instead.

--- src/components/exercise/sample-workout/index.tsx
+++ src/components/exercise/sample-workout/index.tsx
@@ -1,29 +1,52 @@
 import React from 'react';
 import { SampleWorkout } from '@/src/types';
+import { Card } from '@nextui-org/react';
+import GenerateSchema from '../../generate-schema';
 
-interface Props {
+export interface Props {
   sampleWorkout: SampleWorkout;
+  equipment?: string[];
 }
 
-export const ExerciseSampleWorkout: React.FC<Props> = ({ sampleWorkout }) => (
-  <div>
-    <h2>Sample Workout</h2>
-    <p>
-      <strong>Title:</strong>
-      {' '}
-      {sampleWorkout.title}
-    </p>
-    <p>
-      <strong>Description:</strong>
-      {' '}
-      {sampleWorkout.description}
-    </p>
-    <ol>
-      {sampleWorkout.steps.map((step) => (
-        <li key={step}>{step}</li>
-      ))}
-    </ol>
-  </div>
-);
+export const ExerciseSampleWorkout = ({
+  sampleWorkout,
+  equipment,
+}: Props) => {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'ExercisePlan',
+    name: sampleWorkout.title,
+    description: sampleWorkout.description,
+    exercisePlanType: 'http://schema.org/WorkoutPlan',
+    exerciseType: 'http://schema.org/PhysicalActivity',
+    ...(equipment && { equipment: equipment.join(', ') }),
+  };
+  const ExercisePlanSchema = <GenerateSchema jsonLd={jsonLd} />;
+
+  return (
+    <Card className="p-3 rounded-lg shadow-md border border-black-400">
+      <h2 className="text-lg font-medium">{sampleWorkout.title}</h2>
+      <div>
+        <p>{sampleWorkout.description}</p>
+        <h3 className="text-md font-medium">Equipment you will need</h3>
+        <ul>
+          {' '}
+          {equipment ? (
+            equipment.map((item) => <li key={item}>{item}</li>)
+          ) : (
+            <li>Just your body!</li>
+          )}
+        </ul>
+        <h3 className="text-md font-medium">Steps</h3>
+        <ul className="list-disc">
+          {sampleWorkout.steps.map((step) => (
+            <li key={step}>{step}</li>
+          ))}
+        </ul>
+      </div>
+      {ExercisePlanSchema}
+    </Card>
+  );
+};
 
 export default ExerciseSampleWorkout;
